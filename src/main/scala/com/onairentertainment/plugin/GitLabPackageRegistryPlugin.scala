f65cae8acdb1f4ac7e45b59b149e39efc7f85cce
Mikita Seradzinski <nikseredinskiy@gmail.com>
2023-05-23 11:34:38 +0200

    PL-517 Release registry

--- src/main/scala/com/onairentertainment/plugin/GitLabPackageRegistryPlugin.scala
+++ src/main/scala/com/onairentertainment/plugin/GitLabPackageRegistryPlugin.scala
@@ -1,37 +1,53 @@
 package com.onairentertainment.plugin
 
 import lmcoursier.definitions.Authentication
-import lmcoursier.syntax._
+import lmcoursier.syntax.*
 import sbt.Keys.{csrConfiguration, publishMavenStyle, resolvers, updateClassifiers}
 import sbt.librarymanagement.MavenRepository
 import sbt.{AutoPlugin, PluginTrigger, Setting}
+
+import scala.sys.process.Process
 
 object GitLabPackageRegistryPlugin extends AutoPlugin {
 
   val PackageRegistryUri   = "PACKAGES_RW_URI"
   val PackageRegistryToken = "PACKAGES_RW_TOKEN"
-  val CustomAuthHeader     = "Private-Token"
+
+  val PackageLiveRegistryUri   = "PACKAGES_LIVE_RW_URI"
+  val PackageLiveRegistryToken = "PACKAGES_LIVE_RW_TOKEN"
+
+  val CustomAuthHeader = "Private-Token"
 
   override def trigger: PluginTrigger = allRequirements
 
   override def projectSettings: Seq[Setting[_]] = {
-    val uri        = EnvVariableHelper.getRequiredEnvironmentVariable(PackageRegistryUri)
-    val token      = EnvVariableHelper.getRequiredEnvironmentVariable(PackageRegistryToken)
-    val repository = MavenRepository("gitlab", uri)
-    val authentication = Authentication(
-      user     = "user",
-      password = "password",
-      headers  = Seq((CustomAuthHeader, token)),
-      optional = false,
-      realmOpt = None
-    )
 
-    Seq(
-      resolvers += repository,
-      csrConfiguration ~= (_.addRepositoryAuthentication(repository.name, authentication)),
-      updateClassifiers / csrConfiguration ~= (_.addRepositoryAuthentication(repository.name, authentication)),
-      publishMavenStyle := true,
-      aether.AetherKeys.aetherCustomHttpHeaders := Map(CustomAuthHeader -> token)
-    )
+    def prepareSettings(registry: String, registryToken: String): Seq[Setting[_]] = {
+      val uri        = EnvVariableHelper.getRequiredEnvironmentVariable(registry)
+      val token      = EnvVariableHelper.getRequiredEnvironmentVariable(registryToken)
+      val repository = MavenRepository("gitlab", uri)
+      val authentication = Authentication(
+        user     = "user",
+        password = "password",
+        headers  = Seq((CustomAuthHeader, token)),
+        optional = false,
+        realmOpt = None
+      )
+
+      Seq(
+        resolvers += repository,
+        csrConfiguration ~= (_.addRepositoryAuthentication(repository.name, authentication)),
+        updateClassifiers / csrConfiguration := csrConfiguration.value,
+        publishMavenStyle := true,
+        aether.AetherKeys.aetherCustomHttpHeaders := Map(CustomAuthHeader -> token)
+      )
+    }
+
+    val branchName = Process("git rev-parse --abbrev-ref HEAD").lineStream.headOption.getOrElse("unknown")
+
+    if (branchName.startsWith("release"))
+      prepareSettings(PackageLiveRegistryUri, PackageLiveRegistryToken)
+    else
+      prepareSettings(PackageRegistryUri, PackageRegistryToken)
   }
 }

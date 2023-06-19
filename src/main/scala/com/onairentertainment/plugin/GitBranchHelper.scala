74e78886d6df4620f0469a949545ed18cfe3e84c
Mikita Seradzinski <mseradzinski@onairent.live>
2023-06-19 16:43:36 +0200

    PL-525 Removed protected token logic

--- src/main/scala/com/onairentertainment/plugin/GitBranchHelper.scala
+++ src/main/scala/com/onairentertainment/plugin/GitBranchHelper.scala
@@ -7,9 +7,12 @@ object GitBranchHelper {
   private val CiCommitBranch  = "CI_COMMIT_BRANCH"
   private val CiCommitRefName = "CI_COMMIT_REF_NAME"
 
-  private val ReleasePrefix = "release"
+  private val ReleasePrefix       = "release"
+  private val UpdateReleasePrefix = "update/release"
 
   def isReleaseBranch: Boolean = getBranchName.exists(_.startsWith(ReleasePrefix))
+
+  def isUpdateReleaseBranch: Boolean = getBranchName.exists(_.startsWith(UpdateReleasePrefix))
 
   private def getBranchName: Option[EnvVariableHelper.EnvVariable] =
     EnvVariableHelper
